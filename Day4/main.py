d19b2f0465ab778d9c2fc787890c239c528d20bd
PanRagon <ivechr18@student.kristiania.no>
2022-12-04 17:10:57 +0100

    Day 3 & 4

---
+++ Day4/main.py
@@ -0,0 +1,27 @@
+from input import input
+#Part 1
+pairs = input.split()
+
+subsets = 0
+
+for pair in pairs:
+    pair_list = pair.split(',')
+    first_set = set(range(int(pair_list[0].split('-')[0]), int(pair_list[0].split('-')[1])+1))
+    second_set =set(range(int(pair_list[1].split('-')[0]), int(pair_list[1].split('-')[1])+1))
+    if(first_set.issubset(second_set) or set(first_set).issuperset(second_set)):
+        subsets += 1
+
+#Solution Part 1
+print('Part one solution: ', subsets)
+
+#Part 2
+
+intersections = 0
+for pair in pairs:
+    pair_list = pair.split(',')
+    first_set = set(range(int(pair_list[0].split('-')[0]), int(pair_list[0].split('-')[1])+1))
+    second_set =set(range(int(pair_list[1].split('-')[0]), int(pair_list[1].split('-')[1])+1))
+    if(first_set.intersection(second_set)):
+        intersections += 1
+
+print('Part two solution: ', intersections)
